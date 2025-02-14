7f16b8159e3f30156b1bc34f2e8d21ecd83ca0f2
ManojNambiraj <postboxmano@gmail.com>
2025-02-14 19:36:05 +0530

    JS Class1

---
+++ JS/Class1/script.js
@@ -0,0 +1,91 @@
+// Javascript ---> EcmaScript
+
+// Last    ---> ES5
+// Current ---> ES6
+
+// console.log("Hello");
+// console.log("Good evening");
+
+// Variables:
+
+// amount_one_from_dubai = 10000;
+// amountOneFromDubai = 2000;
+// AmountOneFromDubai = 2;
+
+// console.log(amount_one_from_dubai);
+// console.log(amountOneFromDubai);
+// console.log(AmountOneFromDubai);
+
+// Datatypes:
+
+// a = "hello"
+// a = 100
+// a = false
+// let a
+// a = null
+
+
+// console.log(a, typeof a);
+
+// Operators:
+
+    // 1. Arithmetic OP
+
+        // (+, -, *, /, **, %, ++, --)
+
+        // a = 5
+
+        // result = a + 3
+        // result = a - 3
+        // result = a * 3
+        // result = a / 3
+        // result = a ** 3
+        // result = a % 3
+        // result = ++a
+        // result = --a
+
+        // console.log(result);
+    
+    // 2. Assignment OP
+
+        // (=, +=, -=, *=, /=, %=, **=)
+
+        // a = 5
+
+        // a += 7    // a = a + 7
+        // a -= 7    // a = a - 7
+        // a *= 7    // a = a * 7
+        // a /= 7    // a = a / 7
+        // a %= 7    // a = a % 7
+        // a **= 7    // a = a ** 7
+
+        // console.log(a);
+
+    // 3. Comparison OP  --> Returning the Boolean values
+
+        // ( ==, ===, !=, !==, >, >=, <, <=, ? )
+
+        age = 21
+
+        // result = (a == 5)
+        // result = (a === 5)
+        // result = (a != 5)
+        // result = (a !== 5)
+        // result = (a > 5)
+        // result = (a >= 5)
+        // result = (a < 5)
+        // result = (age <= 18)
+        result = (age >= 18) ? "Your Eligible for Voting" : "Not Eligible"
+
+        console.log(result);
+        
+
+    // 4. String OP
+    // 5. Logical OP
+    // 6. Bitwise OP
+    // 7. Ternary OP
+    // 8. Type OP
+
+
+
+
