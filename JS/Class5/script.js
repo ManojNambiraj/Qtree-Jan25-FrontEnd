f94e4abc619bf23c32132a26573a20f9f2e8a248
ManojNambiraj <postboxmano@gmail.com>
2025-03-14 19:20:46 +0530

    JS DOM

---
+++ JS/Class5/script.js
@@ -0,0 +1,33 @@
+// DOM -> Document Object Model
+
+// let ele1 = document.getElementById("demo")
+
+// console.log(ele1);
+
+// ele1.innerHTML = "<h1>Hi</h1>"
+
+// let ele = document.getElementsByClassName("para")
+
+// ele[1].innerHTML = "hi"
+
+
+// let ele2 = document.getElementsByTagName("p")
+
+// ele2[1].innerHTML = "hi";
+
+// ele2[1].style.fontSize = "30px"
+// ele2[1].style.color = "blue"
+
+let newEle = document.createElement("p")
+
+document.body.append(newEle);
+
+// ------------------------------------------
+
+let newEle2 = document.createElement("h1")
+
+newEle2.setAttribute("class", "heading");
+
+newEle2.innerText = "hello world"
+
+newEle.appendChild(newEle2)
