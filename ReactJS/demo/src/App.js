868fdd7cd78ee60c918a4eca15a8b21d2b577020
ManojNambiraj <postboxmano@gmail.com>
2025-03-18 19:27:41 +0530

    react props, css, callback functions

--- ReactJS/demo/src/App.js
+++ ReactJS/demo/src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import ClickBtn from './ClickBtn';
 
 function App() {
+   const handleClick = (value) => {
+     alert(value);
+   };
+
   return (
     <div className="App">
-      <ClickBtn name={"Okay"} />
-      <ClickBtn name={"Music"} />
-      <ClickBtn name={"Books"} />
-      <ClickBtn name={"Sports"} />
-      <ClickBtn name={"AC"} />
-      <ClickBtn name={"DC"} />
-      <ClickBtn name={"XYZ"} />
+      <ClickBtn name={"Okay"} btnColor={"red"} handleClick={handleClick} />
+      <ClickBtn name={"Music"} btnColor={"blue"} handleClick={handleClick} />
+      <ClickBtn name={"Books"} btnColor={"Yellow"} handleClick={handleClick} />
+      <ClickBtn name={"Sports"} btnColor={"Green"} handleClick={handleClick} />
+      <ClickBtn name={"AC"} btnColor={"pink"} handleClick={handleClick} />
+      <ClickBtn name={"DC"} btnColor={"crimson"} handleClick={handleClick} />
+      <ClickBtn
+        name={"XYZ"}
+        btnColor={"yellowGreen"}
+        handleClick={handleClick}
+      />
     </div>
   );
 }
--- ReactJS/demo/src/ClickBtn.js
+++ ReactJS/demo/src/ClickBtn.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./ClickBtn.css";
 
 function ClickBtn(props) {
   return (
     <div>
-      <button>{props.name}</button>
+      <button
+        style={{ backgroundColor: props.btnColor }}
+        // onClick={handleClick}
+        onClick={()=>{props.handleClick(props.name);}}
+      >
+        {props.name}
+      </button>
     </div>
   );
 }
