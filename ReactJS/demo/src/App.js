3582a922adebfbe401e6f60d03ffb6c7a33222df
ManojNambiraj <postboxmano@gmail.com>
2025-03-18 18:44:23 +0530

    React 1st class

---
+++ ReactJS/demo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import ClickBtn from './ClickBtn';
+
+function App() {
+  return (
+    <div className="App">
+      <ClickBtn name={"Okay"} />
+      <ClickBtn name={"Music"} />
+      <ClickBtn name={"Books"} />
+      <ClickBtn name={"Sports"} />
+      <ClickBtn name={"AC"} />
+      <ClickBtn name={"DC"} />
+      <ClickBtn name={"XYZ"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/demo/src/ClickBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ClickBtn(props) {
+  return (
+    <div>
+      <button>{props.name}</button>
+    </div>
+  );
+}
+
+export default ClickBtn;
